cda5c93335174e0e7ea6b142ba7e9823b4f5e997
jiangnenghua <jiangnenghua1992@gmail.com>
2017-12-20 20:53:08 +0800

    null

--- src/main/java/com/fibbery/commons/utils/ReflectUtils.java
+++ src/main/java/com/fibbery/commons/utils/ReflectUtils.java
@@ -1,9 +1,10 @@
 package com.fibbery.commons.utils;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
- * 
+ *
  * @author fibbery
  * @date 2017/12/13
  *
@@ -12,13 +13,13 @@ public class ReflectUtils{
 	/**
 	 * 扫描包下所有类
 	 * @param basePackage 需要扫描的包
-	 * @param clazz  
+	 * @param clazz 接口类
 	 * @return
 	 */
-	public static List<Class> loadClasses(String basePackage,Class<?> clazz){
+	public static List<Class> loadClasses(String basePackage,Class<?> clazz) throws URISyntaxException {
 		String packagePath = basePackage.replace(".", "/");
-		System.out.println(packagePath);
+        
 		return null;
 	}
-	
+
 }
